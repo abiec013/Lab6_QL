ea21a20be502b6da01dbb76089dcf42de5cd9adb
abiec013 <aec1020121@yahoo.com>
2017-10-02 19:07:00 -0400

    second commit

--- Lab6_QL/Program.cs
+++ Lab6_QL/Program.cs
@@ -20,19 +20,19 @@ namespace Lab6_QL
                 Console.WriteLine("Please type a word");
                 string str;
                 str = Console.ReadLine();
-                str = str.ToLower();
-                //converts users text to lower case
+                str = str.ToLower();        //converts users text to lower case
+                
 
-                char[] vowels = { 'a', 'e', 'i', 'o', 'u' };
+                char[] vowels = { 'a', 'e', 'i', 'o', 'u' }; //indicates what characters need to be located
 
-                if (str.IndexOfAny(vowels) ==0)
+                if (str.IndexOfAny(vowels) == 0)
 
                 {
                     Console.WriteLine(str + "way");
 
                 }
 
-                else  
+                else
 
                 {
 
@@ -40,7 +40,7 @@ namespace Lab6_QL
 
                 }
 
-                
+                //user is given two options, to continue or not
 
                 string userChoice;
                 Console.WriteLine("Would you like to translate another word?" + " " + "yes/No");
@@ -57,25 +57,55 @@ namespace Lab6_QL
                     Console.WriteLine("Bye");
                 }
 
-                else
+                else                       
                 {
-                    programContinues = false;
+                    string userChoice1="";      //ensures the user gives a correct answer
+                    bool answer = true;
+                    while (answer == true)
+                    {
+                        Console.WriteLine("Please give a correct answer!");
+                       
+                        userChoice1 = Console.ReadLine();
+                        if (userChoice1 == "yes" || userChoice1 == "no")
+                        {
+                            break; 
+
+                        }
+                        
+                        
+                    }
+
+
+                    if (userChoice1 == "no")
+                        programContinues = false; 
+
+
+
                 }
 
+
+
+
+
+
+
+
+
+
+
+
+
             }
-            
-            
-              
-            
-
-           
-
-            
 
 
 
-           
+
         }
+
+       
+
+
+
 
 
     }
